eec7b775bff7658187464176104ac5bd8006ee17
leanwayne <lean_rap50@hotmail.com>
2025-05-20 19:32:30 -0300

    initial commit

---
+++ assist/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className=' font-bold text-red-400'>hola mundo pepe</p>
+      </div>
+    </>
+  )
+}
+
+export default App
